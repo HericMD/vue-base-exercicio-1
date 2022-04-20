84f3de401ff7ecb93b26ac7e0faa394a20ed648a
HericMD <hericmdms@gmail.com>
2022-04-20 19:30:48 -0300

    'master'

---
+++ src/components/ArtiGo.vue
@@ -0,0 +1,8 @@
+<script>
+export default {};
+</script>
+<script></script>
+<template>
+  <article id="article" class="block">asd</article>
+</template>
+<style></style>
---
+++ src/components/PrinCipal.vue
@@ -0,0 +1,17 @@
+<script>
+import ArtiGo from "@/components/ArtiGo.vue";
+export default {
+  components: { ArtiGo },
+};
+</script>
+<template>
+  <main id="content">
+    <ArtiGo class="b1" />
+    <ArtiGo />
+    <ArtiGo />
+    <ArtiGo />
+    <ArtiGo />
+    <ArtiGo />
+  </main>
+</template>
+<style></style>
